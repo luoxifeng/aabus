d6afb64853b09e7161c6e3aadc7ec530e503dd12
Luo XiFeng <942394682@qq.com>
2021-09-17 02:18:01 +0800

    add filter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const getDefaultAnswer = () => ({ uuid: -1, acks: [] })
+const getDefaultAnswer = () => ({ roundId: -1, acks: [] })
+
+const getAnswer = (target, id) => (target[id] = target[id] || getDefaultAnswer())
 
 module.exports = class AABus {
 
@@ -6,7 +8,22 @@ module.exports = class AABus {
     this.answers = {}
   }
 
-  ask() {
+  afa(blocked) {
+    const aaBus = new AABus()
+    aaBus.answers = this.answers
+    aaBus._blocked = blocked
+    return aaBus
+  }
+
+  askFirstOne(id, info, accept) {
+
+  }
+
+  askLastOne(id, info, accept) {
+
+  }
+
+  askSomeOne(id, info, accept) {
 
   }
 
@@ -14,30 +31,84 @@ module.exports = class AABus {
     if (typeof info === 'function') {
       accept = info
     }
-    let uuid = -1,
-      index = -1,
-      acks,
+    let roundId = -1,
       length;
-    const answer = this.answers[id]
-    if (answer && (acks = answer.acks) && (length = acks.length)) {
-      uuid = ++answer.uuid;
-      for (index = 0; index < length; index++) {
-        acks[index](info, data => accept({ id, uuid, index, data }));
+    const answer = getAnswer(this.answers, id)
+    const _acks = [].concat(answer.acks)
+    const goOn = (acks = _acks) => {
+      if (length = acks.length) {
+        roundId = ++answer.roundId;
+        let i = 0
+        for (let index = 0; index < length; index++) {
+          acks[index](info, data => accept({ 
+            id,
+            roundId,
+            index,
+            data,
+            done: ++i === length
+          }));
+        }
+      } else {
+        accept({ id, roundId: -1, index: -1 });
       }
+    }
+    this._blocked ? this._blocked(_acks, goOn) : goOn()
+  }
+
+  askOneByOne(id, info, accept) {
+    const type = 'askRace'
+    if (typeof info === 'function') {
+      accept = info
+    }
+    let roundId = -1,
+      acks,
+      length,
+      answer;
+    if (
+      (answer = this.answers[id]) &&
+      (acks = answer.acks) &&
+      (length = acks.length)
+    ) {
+      roundId = ++answer.roundId;
+      (function next(preDatas, index) {
+        index < length && acks[index](info, data => {
+          const result = { 
+            id,
+            roundId,
+            type,
+            index,
+            data,
+            preData: preDatas[preDatas.length -1],
+            preDatas,
+            allDatas() {
+              return [...this.preDatas, this.data]
+            },
+          }
+          index++
+          accept(result, {
+            done: index === length,
+            next: () => next([...preDatas, data], index)
+          })
+        });
+      })([], 0)
     } else {
-      accept({ id, uuid: -1, index });
+      accept({ id, roundId: -1, type });
     }
   }
 
-  askOneByOne(id, payload, ) {
+  askOneByOneReverse() {
 
+  }
+
+  askOneByOneRadom() {
+    
   }
 
   askAll(id, info, accept) {
     if (typeof info === 'function') {
       accept = info
     }
-    let uuid = -1,
+    let roundId = -1,
       acks,
       length,
       answer;
@@ -47,16 +118,16 @@ module.exports = class AABus {
       (length = acks.length)
     ) {
       const data = [];
-      uuid = ++answer.uuid;
+      roundId = ++answer.roundId;
       let i = 0
       for (let index = 0; index < length; index++) {
         acks[index](info, t => {
           data[index] = t;
-          ++i === length && accept({ id, uuid, data })
+          ++i === length && accept({ id, roundId, data })
         });
       }
     } else {
-      accept({ id, uuid: -1 });
+      accept({ id, roundId: -1 });
     }
   }
 
@@ -65,7 +136,7 @@ module.exports = class AABus {
     if (typeof info === 'function') {
       accept = info
     }
-    let uuid = -1,
+    let roundId = -1,
       acks,
       length,
       answer;
@@ -74,22 +145,24 @@ module.exports = class AABus {
       (acks = answer.acks) &&
       (length = acks.length)
     ) {
-      uuid = ++answer.uuid;
+      roundId = ++answer.roundId;
       let i = 0
       for (let index = 0; index < length; index++) {
         acks[index](info, data => {
-          ++i === 1 && accept({ id, uuid, type, data })
+          ++i === 1 && accept({ id, roundId, type, data })
         });
       }
     } else {
-      accept({ id, uuid: -1, type });
+      accept({ id, roundId: -1, type });
     }
   }
 
   // 注册应答
-  answer(id, ack) {
-    this.answers[id] = this.answers[id] || getDefaultAnswer()
+  answer(id, meta, ack) {
+    if (typeof meta === 'function') {
+      ack = meta
+    }
+    this.answers[id] = getAnswer(this.answers, id)
     this.answers[id].acks.push(ack)
   }
-
 }
--- test/index.js
+++ test/index.js
@@ -14,14 +14,34 @@ aaBus.answer('test', (info, done) => {
   setTimeout(() => done(3), 3000)
 })
 
-aaBus.askEveryOne('test', (res) => {
-  console.log(res)
-})
+// aaBus.askEveryOne('test', (res) => {
+//   console.log(res)
+// })
 
-aaBus.askAll('test', (res) => {
-  console.log(res)
-})
+// aaBus.askAll('test', (res) => {
+//   console.log(res)
+// })
 
-aaBus.askRace('test', (res) => {
+// aaBus.askRace('test', (res) => {
+//   console.log(res)
+// })
+
+// aaBus.askOneByOne('test', (res, next) => {
+//   console.log(res)
+//   next()
+// })
+
+// aaBus.askOneByOne('test', (res, next) => {
+//   console.log(res)
+//   next()
+// })
+
+aaBus
+.afa((answers, next) => {
+  answers.length = 2
+  next(answers)
+})
+.askEveryOne('test', (res) => {
   console.log(res)
+  // next()
 })
